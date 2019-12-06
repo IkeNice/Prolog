76978d8b390c3601fccb5b3ec81bf242c52a340f
IkeNice <49809282+IkeNice@users.noreply.github.com>
2019-12-06 23:00:50 +0300

    finish 5th task

--- myind5.pl
+++ myind5.pl
@@ -4,6 +4,22 @@ run:- consult('C:/Users/hieut/OneDrive/Документы/Prolog/db.pl'),
     retractall(crew/2),retractall(style/2),
     retractall(dancer/4).
 
+/*
+% показать все
+showAll:-run,
+    writeln(==================================================),
+    writeln("//============= Таблица танцоров =============//"),
+    writeln(==================================================),
+    nl, style(A,B), crew(C,D),
+    dancer(_, A, C, X),
+    write("Имя: "), writeln(X),
+    write("Стиль: "), writeln(B),
+    write("Команда: "), writeln(D), nl, continue,
+    writeln(==================================================),
+    writeln("//============== Таблица стилей ==============//"),
+    writeln(==================================================),
+    nl.
+*/
 % показать танцоров
 showDancers:- run, nl, style(A,B), crew(C,D),
     dancer(_, A, C, X),
@@ -12,11 +28,11 @@ showDancers:- run, nl, style(A,B), crew(C,D),
     write("Команда: "), writeln(D), nl, fail.
 
 % показать стили
-showStyle:-run, nl, style(_,X),
+showStyles:-run, nl, style(_,X),
     write("Стиль: "), writeln(X),nl, fail.
 
 % показать комады
-showCrew:-run, nl, crew(_,X),
+showCrews:-run, nl, crew(_,X),
     write("Название команды: "), writeln(X), nl, fail.
 
 % добавить стиль
@@ -26,13 +42,16 @@ addStyle:-writeln("Добавление стиля"),
     assertz(style(X,Y)),
     tell('C:/Users/hieut/OneDrive/Документы/Prolog/db.pl'),
     listing(crew), listing(style), listing(dancer),
-    told, write('Таблица стилей обновлена'),nl.
+    told, write('Стиль успешно добавлен'),nl.
 
 % добавить команду
 addCrew:-writeln("Добавление команды"),
     write("ID: "), read(X),
     write("Название: "), read(Y),
-    assertz(crew(X,Y)).
+    assertz(crew(X,Y)),
+    tell('C:/Users/hieut/OneDrive/Документы/Prolog/db.pl'),
+    listing(crew), listing(style), listing(dancer),
+    told, write('Команда успешно добавлена'),nl.
 
 % добавить танцора
 addDancer:-writeln("Добавление танцора"),
@@ -40,17 +59,39 @@ addDancer:-writeln("Добавление танцора"),
    write("ID команды: "), read(Y),
    write("ID стиля: "), read(Z),
    write("Имя: "), read(W),
-   assertz(dancer(X,Y,Z,W)).
+   assertz(dancer(X,Y,Z,W)),
+   tell('C:/Users/hieut/OneDrive/Документы/Prolog/db.pl'),
+   listing(crew), listing(style), listing(dancer),
+   told, write('Танцор успешно добавлен'),nl.
 
 % удалить стиль
 delStyle:-writeln("Удалить стиль"),
     write("ID стиля: "),read(X),
     retract(style(X,_)),
-    retract(dancer(_,_,X,_)),
-    writeln("Стиль удален"),
+    retractall(dancer(_,_,X,_)),
+    writeln("Стиль успешно удален"),
     tell('C:/Users/hieut/OneDrive/Документы/Prolog/db.pl'),
     listing(crew), listing(style), listing(dancer),
     told, write('Таблица стилей обновлена'),nl.
+
+% удалить команду
+delCrew:-writeln("Удалить команду"),
+    write("ID команды: "),read(X),
+    retract(crew(X,_)),
+    retractall(dancer(_,X,_,_)),
+    writeln("Команда успешно удалена"),
+    tell('C:/Users/hieut/OneDrive/Документы/Prolog/db.pl'),
+    listing(crew), listing(style), listing(dancer),
+    told, write('Таблица команд обновлена'),nl.
+
+% удалить танцора
+delDancer:-writeln("Удалить танцора"),
+    write("ID танцора: "),read(X),
+    retract(dancer(X,_,_,_)),
+    writeln("Танцор успешно удален"),
+    tell('C:/Users/hieut/OneDrive/Документы/Prolog/db.pl'),
+    listing(crew), listing(style), listing(dancer),
+    told, write('Таблица танцоров обновлена'),nl.
 
 % сохранение базы
 save:- tell('C:/Users/hieut/OneDrive/Документы/Prolog/db.pl'),
